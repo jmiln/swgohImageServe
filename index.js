453e49633d19e1b985fd757b8f4e892de10f3913
jmiln <j.milner359@gmail.com>
2021-11-23 12:35:29 -0800

    Update the /char endpoint to use POST instead of GET

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const config = require("./config.js");
 const express = require("express");
 const puppeteer = require("puppeteer");
+const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const fs = require("fs");
 
@@ -8,28 +10,31 @@ const init = async function() {
     const page = await browser.newPage();
 
     const app = express();
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+
     app.use(express.static(__dirname + "/GearIcons"));
 
     // Set the dimensions of the character image
     await page.setViewport({width: 210, height: 210});
-    const relicTier = [0,0,0,1,2,3,4,5,6,7];
+    const relicTier = [0,0,0,1,2,3,4,5,6,7,8,9];
     const charDef = { defId: "", rarity: 1, level: 0, gear: 1, zeta: 0, relic: 0, side: ""};
 
-    app.get("/char/:charId/:rarity/:level/:gear/:zeta/:relic/:side", async function(req, res) {
-        req.params.relic = relicTier[req.params.relic];
-
+    app.post("/char", async (req, res) => {
         const charStats = {
             unit: {
-                defId:  req.params.charId,
-                rarity: req.params.rarity || charDef.rarity,
-                level:  req.params.level  || charDef.level,
-                gear:   req.params.gear   || charDef.gear,
-                zetas:  req.params.zeta   || charDef.zeta,
-                relic:  req.params.relic  || charDef.relic,
-                side:   req.params.side   || charDef.side
+                defId:   req.body.defId,
+                charUrl: req.body.charUrl,
+                rarity:  req.body.rarity           || charDef.rarity,
+                level:   req.body.level            || charDef.level,
+                gear:    req.body.gear             || charDef.gear,
+                zetas:   req.body.zetas            || charDef.zetas,
+                relic:   relicTier[req.body.relic] || charDef.relic,
+                side:    req.body.side             || charDef.side
             }
         };
-        const ssPath = __dirname + "/imageOut/" + Object.keys(charStats.unit).map(k => charStats.unit[k]).join("-") + ".png";
+        const ssPath = __dirname + "/imageOut/" + Object.keys(charStats.unit).filter(k => k !== "charUrl").map(k => charStats.unit[k]).join("-") + ".png";
+        console.log(ssPath);
         if (fs.existsSync(ssPath)) {
             // File exists, send it
             res.sendFile(ssPath);
@@ -52,7 +57,7 @@ const init = async function() {
         }
     });
 
-    app.listen(3600, () => {console.log("Express server listening");});
+    app.listen(config.port, () => {console.log("Express server listening");});
 };
 
 init();

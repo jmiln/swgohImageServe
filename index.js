6fea95d79107362a3be2139d089fe6d043338a87
jmiln <j.milner359@gmail.com>
2024-08-06 13:42:39 -0700

    chore: Biome fixes & Metadata update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const puppeteer = require("puppeteer");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
-const fs = require("fs");
+const fs = require("node:fs");
 
 // Optimization args from https://www.bannerbear.com/blog/ways-to-speed-up-puppeteer-screenshots/
 const minimal_args = [
@@ -47,12 +47,12 @@ const minimal_args = [
 
 let metadataFile;
 
-const init = async function() {
+const init = async () => {
     const browser = await puppeteer.launch({
         headless: true,
         // headless: "new",
         args: minimal_args,
-        userDataDir: "./cacheDir"
+        userDataDir: "./cacheDir",
     });
     const page = await browser.newPage();
 
@@ -60,44 +60,51 @@ const init = async function() {
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
 
-    app.use(express.static(__dirname + "/public"));
+    app.use(express.static(`${__dirname}/public`));
 
-    const relicTier = [0,0,0,1,2,3,4,5,6,7,8,9];
-    const charDef = { defId: "", rarity: 1, level: 0, gear: 1, zeta: 0, relic: 0, side: "", omicron: 0};
+    const relicTier = [0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const charDef = { defId: "", rarity: 1, level: 0, gear: 1, zeta: 0, relic: 0, side: "", omicron: 0 };
 
     // Update the metadata file every hour so we can keep the assetVersion current
     await updateMetaData();
-    setInterval(async () => {
-        await updateMetaData();
-    }, 60 * 60 * 1000);
+    setInterval(
+        async () => {
+            await updateMetaData();
+        },
+        60 * 60 * 1000,
+    );
 
     app.post("/char", async (req, res) => {
         // Set the dimensions of the character image
-        await page.setViewport({width: 210, height: 210});
+        await page.setViewport({ width: 210, height: 210 });
         const charStats = {
-            defId:   req.body.defId,
-            charUrl: `http://localhost:${config.port}/CharIcons/` + await checkImgOrDownload(req.body.charUrl, "./public/CharIcons"),
-            rarity:  req.body.rarity           || charDef.rarity,
-            level:   req.body.level            || charDef.level,
-            gear:    req.body.gear             || charDef.gear,
-            zetas:   req.body.zetas            || charDef.zetas,
-            relic:   relicTier[req.body.relic] || charDef.relic,
-            omicron: req.body.omicron          || charDef.omicron,
-            side:    req.body.side             || charDef.side
+            defId: req.body.defId,
+            charUrl: `http://localhost:${config.port}/CharIcons/${await checkImgOrDownload(req.body.charUrl, "./public/CharIcons")}`,
+            rarity: req.body.rarity || charDef.rarity,
+            level: req.body.level || charDef.level,
+            gear: req.body.gear || charDef.gear,
+            zetas: req.body.zetas || charDef.zetas,
+            relic: relicTier[req.body.relic] || charDef.relic,
+            omicron: req.body.omicron || charDef.omicron,
+            side: req.body.side || charDef.side,
         };
-        ejs.renderFile(__dirname + "/ejs/char.ejs", {
-            baseURL: `http://localhost:${config.port}`,
-            char: charStats
-        }, async (err, result) => {
-            if (err) return console.log("info", "error encountered: " + err);
-            await page.setContent(result, {
-                waitUntil: ["load"]
-            });
-            await page.addStyleTag({path: "./public/css/styles.css"});
-            const ssBuffer = await page.screenshot({type: "png", omitBackground: true});
-            res.contentType("image/png");
-            res.send(Buffer.from(ssBuffer, "binary"));
-        });
+        ejs.renderFile(
+            `${__dirname}/ejs/char.ejs`,
+            {
+                baseURL: `http://localhost:${config.port}`,
+                char: charStats,
+            },
+            async (err, result) => {
+                if (err) return console.log("info", `error encountered: ${err}`);
+                await page.setContent(result, {
+                    waitUntil: ["load"],
+                });
+                await page.addStyleTag({ path: "./public/css/styles.css" });
+                const ssBuffer = await page.screenshot({ type: "png", omitBackground: true });
+                res.contentType("image/png");
+                res.send(Buffer.from(ssBuffer, "binary"));
+            },
+        );
     });
 
     // app.get("/chart", async (req, res) => {
@@ -163,33 +170,33 @@ const init = async function() {
         if (!Array.isArray(charListIn)) return console.error("[panic] Character list is not an array");
         for (const thisChar of charListIn) {
             unitList.push({
-                defId:   thisChar.defId,
-                charUrl: `http://localhost:${config.port}/CharIcons/` + await checkImgOrDownload(thisChar.charUrl, "./public/CharIcons"),
-                name:    thisChar?.name,
-                rarity:  thisChar.rarity           || charDef.rarity,
-                gear:    thisChar.gear             || charDef.gear,
-                relic:   relicTier[thisChar.relic] || charDef.relic,
-                side:    thisChar.side             || charDef.side,
-                gp:      thisChar.gp               || "N/A",
+                defId: thisChar.defId,
+                charUrl: `http://localhost:${config.port}/CharIcons/${await checkImgOrDownload(thisChar.charUrl, "./public/CharIcons")}`,
+                name: thisChar?.name,
+                rarity: thisChar.rarity || charDef.rarity,
+                gear: thisChar.gear || charDef.gear,
+                relic: relicTier[thisChar.relic] || charDef.relic,
+                side: thisChar.side || charDef.side,
+                gp: thisChar.gp || "N/A",
 
                 // The required tiers that a character needs to get past
-                gpReq:   thisChar.gpReq            || 0,
-                rarityReq: thisChar.rarityReq      || 0,
-                gearReq:   thisChar.gearReq        || 0,
-                relicReq:  thisChar.relicReq       || 0,
-                valid: thisChar.isValid            || false,
+                gpReq: thisChar.gpReq || 0,
+                rarityReq: thisChar.rarityReq || 0,
+                gearReq: thisChar.gearReq || 0,
+                relicReq: thisChar.relicReq || 0,
+                valid: thisChar.isValid || false,
 
                 // Mark as a ship, and/ or as required
-                ship: thisChar.isShip              || false,
-                required: thisChar.isRequired      || false,
+                ship: thisChar.isShip || false,
+                required: thisChar.isRequired || false,
             });
         }
 
         const unitsOut = {};
-        if (unitList.find(u => !u.ship)) unitsOut.charList = unitList.filter(u => !u.ship);
-        if (unitList.find(u => u.ship))  unitsOut.shipList = unitList.filter(u => u.ship);
+        if (unitList.find((u) => !u.ship)) unitsOut.charList = unitList.filter((u) => !u.ship);
+        if (unitList.find((u) => u.ship)) unitsOut.shipList = unitList.filter((u) => u.ship);
 
-        const isRequired = unitList.find(u => u.required) ? true : false;
+        const isRequired = !!unitList.find((u) => u.required);
 
         let headerHeight = 0;
         if (req.body?.header) {
@@ -202,33 +209,34 @@ const init = async function() {
         const maxWidth = 1168;
 
         // Figure out the needed height
-        const maxHeight = 40
-            + ((Object.keys(unitsOut).length-1) * 30)                               // The amount of spacers needed
-            + ((unitList.length+1) * charRowHeight)                                 // The number of units
-            + (req?.body?.lastUpdated ? 55 : 0)                                     // Space for a footer
-            + headerHeight                                                          // Header spacing
-            + (isRequired ? 30 : 0)                                                 // Extra for the required unit description
-            + (unitsOut?.charList?.length && unitsOut?.shipList?.length ? 40 : 0);  // Add space for the spacer between char & ship if needed
-        await page.setViewport({width: maxWidth, height: maxHeight});
+        const maxHeight =
+            40 +
+            (Object.keys(unitsOut).length - 1) * 30 + // The amount of spacers needed
+            (unitList.length + 1) * charRowHeight + // The number of units
+            (req?.body?.lastUpdated ? 55 : 0) + // Space for a footer
+            headerHeight + // Header spacing
+            (isRequired ? 30 : 0) + // Extra for the required unit description
+            (unitsOut?.charList?.length && unitsOut?.shipList?.length ? 40 : 0); // Add space for the spacer between char & ship if needed
+        await page.setViewport({ width: maxWidth, height: maxHeight });
 
         const objIn = {
             baseURL: `http://localhost:${config.port}`,
             maxCharWidth: maxWidth,
             header: req.body.header,
             units: unitsOut,
-            required: isRequired
+            required: isRequired,
         };
         if (req.body.lastUpdated) {
             objIn.footer = `Last updated ${new Date(req.body.lastUpdated).toUTCString()}`;
         }
 
-        ejs.renderFile(__dirname + "/ejs/panicReq.ejs", objIn, async (err, result) => {
-            if (err) return console.log("info", "error encountered: " + err);
+        ejs.renderFile(`${__dirname}/ejs/panicReq.ejs`, objIn, async (err, result) => {
+            if (err) return console.log("info", `error encountered: ${err}`);
             await page.setContent(result, {
-                waitUntil: ["load"]
+                waitUntil: ["load"],
             });
-            await page.addStyleTag({path: "./public/css/styles.css"});
-            const ssBuffer = await page.screenshot({type: "png", omitBackground: true});
+            await page.addStyleTag({ path: "./public/css/styles.css" });
+            const ssBuffer = await page.screenshot({ type: "png", omitBackground: true });
             res.contentType("image/png");
             res.send(Buffer.from(ssBuffer, "binary"));
         });
@@ -245,46 +253,47 @@ const init = async function() {
         if (!Array.isArray(charListIn)) return console.error("[multi-char] Character list is not an array");
         for (const thisChar of charListIn) {
             charList.push({
-                defId:   thisChar.defId,
-                charUrl: `http://localhost:${config.port}/CharIcons/` + await checkImgOrDownload(thisChar.charUrl, "./public/CharIcons"),
-                name:    thisChar.name,
-                rarity:  thisChar.rarity           || charDef.rarity,
-                level:   thisChar.level            || charDef.level,
-                gear:    thisChar.gear             || charDef.gear,
-                zetas:   thisChar.zetas            || charDef.zetas,
-                relic:   relicTier[thisChar.relic] || charDef.relic,
-                omicron: thisChar.omicron          || charDef.omicron,
-                side:    thisChar.side             || charDef.side
+                defId: thisChar.defId,
+                // biome-ignore lint/style/useTemplate: <explanation>
+                charUrl: `http://localhost:${config.port}/CharIcons/` + (await checkImgOrDownload(thisChar.charUrl, "./public/CharIcons")),
+                name: thisChar.name,
+                rarity: thisChar.rarity || charDef.rarity,
+                level: thisChar.level || charDef.level,
+                gear: thisChar.gear || charDef.gear,
+                zetas: thisChar.zetas || charDef.zetas,
+                relic: relicTier[thisChar.relic] || charDef.relic,
+                omicron: thisChar.omicron || charDef.omicron,
+                side: thisChar.side || charDef.side,
             });
         }
 
         const maxPerRow = 8;
 
         // Set the dimensions of the character image
-        const maxCharWidth  = charList.length > maxPerRow ? maxPerRow : charList.length;
+        const maxCharWidth = charList.length > maxPerRow ? maxPerRow : charList.length;
         const maxCharHeight = Math.ceil(charList.length / maxPerRow);
         const maxWidth = 200 * maxCharWidth;
-        const maxHeight = 55 + (250 * maxCharHeight) + (req?.body?.lastUpdated ? 55 : 0);
-        await page.setViewport({width: maxWidth, height: maxHeight});
+        const maxHeight = 55 + 250 * maxCharHeight + (req?.body?.lastUpdated ? 55 : 0);
+        await page.setViewport({ width: maxWidth, height: maxHeight });
 
         const objIn = {
             baseURL: `http://localhost:${config.port}`,
             maxCharWidth: maxWidth,
             header: req.body.header,
             characters: charList,
-            footer: ""
+            footer: "",
         };
         if (req.body.lastUpdated) {
             objIn.footer = `Last updated ${new Date(req.body.lastUpdated).toUTCString()}`;
         }
 
-        ejs.renderFile(__dirname + "/ejs/multi-char.ejs", objIn, async (err, result) => {
-            if (err) return console.log("info", "error encountered: " + err);
+        ejs.renderFile(`${__dirname}/ejs/multi-char.ejs`, objIn, async (err, result) => {
+            if (err) return console.log("info", `error encountered: ${err}`);
             await page.setContent(result, {
-                waitUntil: ["load"]
+                waitUntil: ["load"],
             });
-            await page.addStyleTag({path: "./public/css/styles.css"});
-            const ssBuffer = await page.screenshot({type: "png", omitBackground: true});
+            await page.addStyleTag({ path: "./public/css/styles.css" });
+            const ssBuffer = await page.screenshot({ type: "png", omitBackground: true });
             res.contentType("image/png");
             res.send(Buffer.from(ssBuffer, "binary"));
         });
@@ -309,15 +318,16 @@ async function checkImgOrDownload(url, dir) {
 
     let assetUrl;
     if (config.assetPort) {
-        const assetName = imgName.split(".")[1];    // Generally `tex.ASSET_NAME.png`, so grabbing just the name out
-        assetUrl  = `http://localhost:${config.assetPort}/Asset/single?forceReDownload=true&version=${assetVersion}&assetName=${assetName}`;
+        const assetName = imgName.split(".")[1]; // Generally `tex.ASSET_NAME.png`, so grabbing just the name out
+        assetUrl = `http://localhost:${config.assetPort}/Asset/single?forceReDownload=true&version=${assetVersion}&assetName=${assetName}`;
     }
 
     // Otherwise, download & save the image, then give the name
     const buffer = await fetch(assetUrl || url)
-        .then(async res => res.blob())
-        .then(async res => res.arrayBuffer())
-        .then(async res => Buffer.from(res));
+        .then(async (res) => res.blob())
+        .then(async (res) => res.arrayBuffer())
+        .then(async (res) => Buffer.from(res))
+        .catch((err) => console.error(err));
     await fs.promises.writeFile(path, buffer);
     return imgName;
 }
@@ -325,7 +335,7 @@ async function checkImgOrDownload(url, dir) {
 // Update the metadata file if it exists, create it otherwise
 const ComlinkStub = require("@swgoh-utils/comlink");
 const comlinkStub = new ComlinkStub(config.clientStub);
-const META_FILE = __dirname + "/data/metadata.json";
+const META_FILE = `${__dirname}/data/metadata.json`;
 const META_KEYS = ["assetVersion", "latestGamedataVersion", "latestLocalizationBundleVersion"];
 async function updateMetaData() {
     const meta = await comlinkStub.getMetaData();
@@ -343,7 +353,7 @@ async function updateMetaData() {
     }
 
     if (isUpdated) {
-        fs.writeFileSync(META_FILE, JSON.stringify(metaOut), {encoding: "utf8"});
+        fs.writeFileSync(META_FILE, JSON.stringify(metaOut), { encoding: "utf8" });
     }
     metadataFile = metaOut;
 
